199e8872c0dfc0d17d860df5fc4cb7b32bfc7d3b
Frédéric CORNU <fcornu@wardsback.org>
2021-03-03 10:13:26 +0100

    add INFO log when registering new device

--- src/model/setup.cpp
+++ src/model/setup.cpp
@@ -6,6 +6,7 @@ void Setup::addDevice(const Device* pDevice) {
 
     if( ! _devicesMap.contains(pDevice->getName()) ) {
         _devicesMap[pDevice->getName()] = pDevice;
+        BOOST_LOG_TRIVIAL(info) << "New device added: " << pDevice->getName();
     } else {
         BOOST_LOG_TRIVIAL(warning) << "Duplicate device name: " << pDevice->getName() << ". This should not happen";
     }
