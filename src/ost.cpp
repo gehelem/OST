052ddf4b66e50aacfc2e412bd3861646047118b1
Frédéric CORNU <fcornu@wardsback.org>
2021-03-05 14:14:48 +0100

    bump down base application class to QCoreApplication

--- src/ost.cpp
+++ src/ost.cpp
@@ -1,4 +1,4 @@
-#include <QApplication>
+#include <QCoreApplication>
 #include <boost/log/trivial.hpp>
 
 #include "controller.h"
@@ -10,10 +10,10 @@
 int main(int argc, char *argv[])
 {
     BOOST_LOG_TRIVIAL(info) << "OST starting up";
-    QApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
     Controller controller(&a);
     Q_UNUSED(controller);
-    int nAppReturnCode = QApplication::exec();
+    int nAppReturnCode = QCoreApplication::exec();
     BOOST_LOG_TRIVIAL(info) << "OST app terminated with status : " << nAppReturnCode;
     return nAppReturnCode;
 
