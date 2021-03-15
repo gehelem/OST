6e452e782dfd0db92792d4fd98f60acf1578f520
Frédéric CORNU <fcornu@wardsback.org>
2021-03-15 08:27:42 +0100

    introduce JSon dumper

---
+++ src/utils/jsondumper.h
@@ -0,0 +1,30 @@
+//
+// Created by deufrai on 12/03/2021.
+//
+
+#ifndef OST_JSONDUMPER_H
+#define OST_JSONDUMPER_H
+
+#include <QString>
+#include "propertyvisitor.h"
+#include "model/property.h"
+
+class JSonDumper : public PropertyVisitor {
+
+
+public:
+    void visit(NumberProperty *pProperty) override;
+    void visit(SwitchProperty *pProperty) override;
+    void visit(TextProperty *pProperty) override;
+    void visit(LightProperty *pProperty) override;
+
+    [[nodiscard]] inline const QString& getResult() const { return _result; }
+
+private:
+    QString _result;
+
+    QJsonObject dumpPropertyCommons(Property* pProperty);
+};
+
+
+#endif //OST_JSONDUMPER_H
