25200749043bbe688d11e97a68ebb830c14c3d24
gehelem <gilles@joag.fr>
2022-09-09 14:53:58 +0200

    simplification

--- src/modules/basemodule/basemodule.cpp
+++ src/modules/basemodule/basemodule.cpp
@@ -247,11 +247,7 @@ QVariantMap Basemodule::getProfile(void)
 void Basemodule::OnExternalEvent(const QString &eventType, const QString  &eventModule, const QString  &eventKey, const QVariantMap &eventData)
 {
 
-    if ( (eventType=="dump")&&(eventModule=="*") ) {
-        emit moduleEvent("moduledump",_modulename,"*",_ostproperties);
-        return;
-    }
-    if ( (eventType=="dump")&&(eventModule==_modulename) ) {
+    if ( (eventType=="dump")&&((eventModule=="*")||(eventModule==_modulename)) ) {
         emit moduleEvent("moduledump",_modulename,"*",_ostproperties);
         return;
     }
