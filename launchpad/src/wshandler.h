9f29d1c954b2f02d369ac8305fee29493d47f3f0
gehelem <gilles@joag.fr>
2022-12-30 17:00:36 +0100

    first attempt

---
+++ launchpad/src/wshandler.h
@@ -0,0 +1,39 @@
+#ifndef WSHANDLER_h_
+#define WSHANDLER_h_
+#include "QtWebSockets/qwebsocketserver.h"
+#include "QtWebSockets/qwebsocket.h"
+
+QT_FORWARD_DECLARE_CLASS(QWebSocketServer)
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
+/*!
+ * Websocket management
+ * open/close/send/receives sockets
+ * it should translate JSON messages
+ */
+class WShandler : public QObject
+{
+    Q_OBJECT
+public:
+    WShandler(QObject *parent);
+    ~WShandler();
+public :
+    void processModuleEvent(const QString &eventType, const QString  &eventModule, const QString  &eventKey, const QVariantMap &eventData);
+
+
+signals:
+    void closed();
+    void textRcv(QString txt);
+    void externalEvent(const QString &eventType, const QString  &eventModule, const QString  &eventKey, const QVariantMap &eventData);
+private:
+    void onNewConnection();
+    void processTextMessage(QString message);
+    void processBinaryMessage(QByteArray message);
+    void sendmessage(QString message);
+    void sendJsonMessage(QJsonObject json);
+    void sendbinary(QByteArray *data);
+    void socketDisconnected();
+    QWebSocketServer *m_pWebSocketServer;
+    QList<QWebSocket *> m_clients;
+
+};
+#endif
