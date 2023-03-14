f77a97cf188447caca16e0615e7f2297dfe72110
gehelem <gilles@joag.fr>
2023-03-14 18:22:20 +0100

    another  yes prompt flag

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository --yes ppa:gehelem/ostserver-daily
 sudo add-apt-repository --yes ppa:mutlaqja/ppa
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install -y libindi1 libstellarsolver ostserver ostmodules nginx gsc pip
 
 sudo mkdir /usr/share/astrometry
