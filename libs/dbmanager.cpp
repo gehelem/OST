2d6a41aa4947e6ac2ebea00f77beffabe58c0969
gehelem <gilles@joag.fr>
2023-03-19 12:55:42 +0100

    don't keep db connection opened

--- libs/dbmanager.cpp
+++ libs/dbmanager.cpp
@@ -21,13 +21,13 @@ bool DBManager::dbInit(const QString &pDbPath, const QString &pConnectionName)
         mDb = QSqlDatabase::addDatabase("QSQLITE", pConnectionName);
         mDb.setDatabaseName(mDbPath + "ost.db" );
         mQuery = QSqlQuery(mDb);
-
         if(!mDb.open())
         {
             sendError("dbOpen - ERROR: " + mDb.databaseName() + " - " + mDb.lastError().text());
             return false;
         }
         if (!mDbExists) CreateDatabaseStructure();
+        mDb.close();
         return true;
     }
     else
@@ -41,6 +41,7 @@ bool DBManager::dbInit(const QString &pDbPath, const QString &pConnectionName)
 
 void DBManager::CreateDatabaseStructure()
 {
+
     sendMessage("OST database creation with default values");
     QFile _file;
     _file.setFileName(":db.sql");
@@ -64,9 +65,16 @@ void DBManager::CreateDatabaseStructure()
 
     }
 
+
 }
 bool DBManager::setDbProfile(const QString &pModuleType, const QString &pProfileName, QVariantMap &profile )
 {
+    if(!mDb.open())
+    {
+        sendError("dbOpen - ERROR: " + mDb.databaseName() + " - " + mDb.lastError().text());
+        return false;
+    }
+
     QJsonObject  obj = QJsonObject::fromVariantMap(profile);
     QJsonDocument doc(obj);
     QByteArray docByteArray = doc.toJson(QJsonDocument::Compact);
@@ -77,35 +85,54 @@ bool DBManager::setDbProfile(const QString &pModuleType, const QString &pProfile
     {
         sendError("setProfile - ERROR SQL =" + sql);
         sendError("setProfile - ERROR : " + mQuery.lastError().text());
+        mDb.close();
         return false;
     }
+    mDb.close();
     return true;
+
 }
 bool DBManager::getDbProfile(const QString &pModuleType, const QString &pProfileName, QVariantMap &result )
 {
+    if(!mDb.open())
+    {
+        sendError("dbOpen - ERROR: " + mDb.databaseName() + " - " + mDb.lastError().text());
+        return false;
+    }
     QString _sql = "SELECT ALLVALUES FROM PROFILES WHERE MODULETYPE='" + pModuleType + "' AND PROFILENAME='" + pProfileName +
                    "'";
     if (!mQuery.exec(_sql))
     {
         sendError("getProfile - ERROR SQL =" + _sql);
         sendError("getProfile - ERROR : " + mQuery.lastError().text());
+        mDb.close();
+        return false;
     }
     while (mQuery.next())
     {
         QJsonDocument res = QJsonDocument::fromJson(mQuery.value(0).toString().toUtf8());
         QJsonObject  obj = res.object();
         result = obj.toVariantMap();
+        mDb.close();
         return true;
     }
+    mDb.close();
     return false;
 }
 bool DBManager::getDbProfiles(QString moduleType, QVariantMap &result )
 {
+    if(!mDb.open())
+    {
+        sendError("dbOpen - ERROR: " + mDb.databaseName() + " - " + mDb.lastError().text());
+        return false;
+    }
     QString _sql = "SELECT PROFILENAME,ALLVALUES FROM PROFILES WHERE MODULETYPE='" + moduleType + "' ";
     if (!mQuery.exec(_sql))
     {
         sendError("getProfiles - ERROR SQL =" + _sql);
         sendError("getProfiles - ERROR : " + mQuery.lastError().text());
+        mDb.close();
+        return false;
     }
     while (mQuery.next())
     {
@@ -115,15 +142,23 @@ bool DBManager::getDbProfiles(QString moduleType, QVariantMap &result )
         QVariantMap line = obj.toVariantMap();
         result [name] = line;
     }
+    mDb.close();
     return true;
 }
 bool DBManager::getDbConfiguration(QString configName, QVariantMap &result )
 {
+    if(!mDb.open())
+    {
+        sendError("dbOpen - ERROR: " + mDb.databaseName() + " - " + mDb.lastError().text());
+        return false;
+    }
     QString _sql = "SELECT MODULENAME,MODULETYPE,PROFILENAME FROM CONFIGURATIONS WHERE CONFIGNAME='" + configName + "'";
     if (!mQuery.exec(_sql))
     {
         sendError("getConfiguration - ERROR SQL =" + _sql);
         sendError("getConfiguration - ERROR : " + mQuery.lastError().text());
+        mDb.close();
+        return false;
     }
     while (mQuery.next())
     {
@@ -132,5 +167,6 @@ bool DBManager::getDbConfiguration(QString configName, QVariantMap &result )
         _line["profilename"] = mQuery.value(2).toString().toUtf8();
         result[mQuery.value(0).toString().toUtf8()] = _line;
     }
+    mDb.close();
     return true;
 }
