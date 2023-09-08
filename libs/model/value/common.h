14be0eb9a0890c39619059651482e894364b7420
gehelem <gilles@joag.fr>
2023-09-09 00:14:29 +0200

    Remove fat

--- libs/model/value/common.h
+++ libs/model/value/common.h
@@ -31,108 +31,13 @@ inline int StateToInt(State val )
     qDebug() << "StateToInt Cant convert state - return State::Error";
     return 3;
 }
+
 typedef enum
 {
     Info = 0,
     Warn,
     Err
 } MsgLevel;
-
-template <typename U>
-class ListOfValues
-{
-    public:
-        QMap<U, QString> getList()
-        {
-            return list;
-        }
-        bool add(U val, QString label)
-        {
-            if (list.contains(val))
-            {
-                qDebug() << "ListOfValues - add - key " << val << " already exists (" << list[val] << ").";
-                return false;
-            }
-            list[val] = label;
-            return true;
-        }
-        bool update(U val, QString label)
-        {
-            if (!list.contains(val))
-            {
-                qDebug() << "ListOfValues - update - key " << val << " doesn't exist (" << list[val] << ").";
-                return false;
-            }
-            list[val] = label;
-            return true;
-        }
-        bool del(U val)
-        {
-            if (!list.contains(val))
-            {
-                qDebug() << "ListOfValues - del - key " << val << " does not exist.";
-                return false;
-            }
-            list.remove(val);
-            return true;
-        }
-        bool clear()
-        {
-            list.clear();
-            return true;
-        }
-
-    private:
-        QMap<U, QString> list;
-};
-
-template <typename U>
-class Grid
-{
-    public:
-        QList<U> getGrid()
-        {
-            return mGridValues;
-        }
-        bool add(U val)
-        {
-            mGridValues.append(val);
-            return true;
-        }
-        bool del(int i)
-        {
-            if (i >= mGridValues.size())
-            {
-                qDebug() << "Grid - del - trying to delete item " << i << " while grid size is " << mGridValues.size();
-                return false;
-            }
-            mGridValues.removeAt(i);
-            return true;
-        }
-        bool update(U val, int i)
-        {
-            if (i >= mGridValues.size())
-            {
-                qDebug() << "Grid - update - trying to update item " << i << " while grid size is " << mGridValues.size();
-                return false;
-            }
-            mGridValues[i] = val;
-            return true;
-        }
-        bool clear()
-        {
-            mGridValues.clear();
-            return true;
-        }
-        void setGridValues(QList<U> vals )
-        {
-            mGridValues.clear();
-            mGridValues = vals;
-        }
-    private:
-        QList<U> mGridValues;
-
-};
 
 typedef struct ImgData
 {
