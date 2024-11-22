0a7801c828d280b6bf4b2b07f3a72731b1f397b6
gehelem <gilles@joag.fr>
2024-11-22 14:55:43 +0100

    add update script

---
+++ update_daily.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y ostserver ostmodules
+
+#get ostserver front
+cd /var/www/ostserver
+sudo wget https://github.com/gehelem/ost-front/releases/download/WorkInProgress/html.tar.gz --no-check-certificate
+sudo tar -xvf html.tar.gz
+#grant access
+sudo chown -R www-data:www-data /var/www/ostserver
+
+sudo service nginx restart
+sudo service ostserver restart
