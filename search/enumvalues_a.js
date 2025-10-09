472e4fd6fb61df7687f13561ad0b4ea431a9b125
gehelem <gilles@joag.fr>
2025-10-09 10:28:47 +0000

    Deploying to gh-pages from @ gehelem/OST@8e2e0993b54d43e10adf9497c2b5d607800913f6 🚀

--- search/enumvalues_a.js
+++ search/enumvalues_a.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['sliderandvalue_0',['SliderAndValue',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a81981a99b5110ce75695f7a376b1136a',1,'OST']]],
-  ['slidernone_1',['SliderNone',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a3141ca269caf89a5330ddf85ee458e18',1,'OST']]],
-  ['slideronly_2',['SliderOnly',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a041c20ba72b0b02fb32a5ccd6d1eaa23',1,'OST']]],
-  ['spinner_3',['spinner',['../namespaceOST.html#a4f0f6d56fa87488f90f09dc5a77ecfd7a14878ee4737a35c57507a4fd28f7d9ef',1,'OST']]]
+  ['sdy_0',['SDY',['../namespaceOST.html#a2bd59f1bb3735729a6381f7e5ec5970cad556fd81bf4fb5d95ef013b8bf9affd2',1,'OST']]],
+  ['sliderandvalue_1',['SliderAndValue',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a81981a99b5110ce75695f7a376b1136a',1,'OST']]],
+  ['slidernone_2',['SliderNone',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a3141ca269caf89a5330ddf85ee458e18',1,'OST']]],
+  ['slideronly_3',['SliderOnly',['../namespaceOST.html#ad6965fa6b47814dd2ed9a7215368f131a041c20ba72b0b02fb32a5ccd6d1eaa23',1,'OST']]],
+  ['spinner_4',['spinner',['../namespaceOST.html#a4f0f6d56fa87488f90f09dc5a77ecfd7a14878ee4737a35c57507a4fd28f7d9ef',1,'OST']]],
+  ['sxy_5',['SXY',['../namespaceOST.html#a2bd59f1bb3735729a6381f7e5ec5970cae871ecb3128117978e4ade283b3fcd63',1,'OST']]]
 ];
