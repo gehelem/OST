5d4a3aad51cfbe42440a1be9ab0777e29f5386c6
gehelem <gilles@joag.fr>
2025-12-05 09:43:43 +0100

    Handle exit code

--- CI/CI-build.sh
+++ CI/CI-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
 # CI - build job
-# 
+#
+set -e
 apt-get update
 apt-get clean
 apt-get install -y software-properties-common
 apt-add-repository ppa:mutlaqja/ppa
+apt-get update
 apt-get install -y build-essential cmake git npm qtbase5-dev libqt5websockets5-dev libindi-dev libstellarsolver-dev libnova-dev libgsl-dev wcslib-dev libcfitsio-dev libavahi-client-dev libavahi-common-dev zlib1g-dev dpkg-dev qtdeclarative5-dev libqt5svg5-dev libkf5doctools-dev libkf5config-dev libkf5guiaddons-dev libkf5i18n-dev libkf5newstuff-dev libkf5notifications-dev libkf5xmlgui-dev libkf5plotting-dev libkf5crash-dev libkf5notifyconfig-dev libeigen3-dev libraw-dev libsecret-1-dev debhelper devscripts lintian autotools-dev intltool-debian extra-cmake-modules
 mkdir media
 dpkg-buildpackage -us -uc 
